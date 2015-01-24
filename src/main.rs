05bb63125fc244d76025f7be9b746245ef1aacbe
Liam Marshall <archimedespi3141@gmail.com>
2015-01-24 09:52:42 -0600

    config file test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[macro_use] extern crate log;
+extern crate toml;
+use std::io::{File};
+
 fn main() {
-    println!("Hello, world!");
+	info!("tinshel starting");
+	debug!("now load config");
+	let configpath = Path::new("/home/liam/.tinshel.toml");
+
+
+	let configcontents = match File::open(&configpath).read_to_string() {
+        Ok(s) => s,
+        Err(e) => panic!("error reading file: {}", e),
+    };
+
+    //println!("{}", configcontents);
+	let config = toml::Parser::new(configcontents.as_slice()).parse().unwrap();
+	debug!("{:?}", config);
 }
